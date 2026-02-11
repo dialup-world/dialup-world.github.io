03d6e0d7172cb9d0bb8ccc32e7e04965eb17dca8
Mike Dank <mike@philtel.org>
2026-02-11 16:20:20 -0500

    Change to no style please theme

--- blog/index.markdown
+++ blog/index.markdown
@@ -1,5 +1,3 @@
 ---
 layout: home
 ---
-
-{% include footer.html %}
--- blog/serve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run \
-  --volume="$PWD:/srv/jekyll:Z" \
-  --publish 4000:4000 \
-  jekyll/jekyll:3.8.6 \
-  jekyll serve
